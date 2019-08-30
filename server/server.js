e61c85905e39bb6cef6c39f7bf3c6cc42f3a0a59
Vlad Rez <vladrezdev@gmail.com>
2019-08-30 16:15:48 -0700

    user root query

--- server/server.js
+++ server/server.js
@@ -1,27 +1,36 @@
-const express = require("express")
-const db = require("../config/keys").MONGO_URI
-const mongoose = require("mongoose")
+const models = require("./models/index");
+const express = require("express");
+const db = require("../config/keys").MONGO_URI;
+const mongoose = require("mongoose");
 
-const bodyParser = require("body-parser")
-const cors = require("cors")
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
-const expressGraphQL = require("express-graphql")
-
+const expressGraphQL = require("express-graphql");
+const schema = require("./schema/schema");
 
 const app = express();
-// app.use(cors());
+app.use(cors());
 
-// app.
+app.use(bodyParser.json());
 
+app.use("/graphql", expressGraphQL(req => {
+    return {
+        schema,
+        context: {
+            token: req.headers.authorization
+        },
+        graphiql: true
+    };
+}));
 
 if (!db) {
-    throw new Error("No Database Connection URI")
+  throw new Error("No Database Connection URI");
 }
 
 mongoose
-    .connect(db, {useNewUrlParser: true})
-    .then(()=> console.log("Connected to MongoDB"))
-    .catch(err => console.log(err))
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch(err => console.log(err));
 
-
-module.exports = app
+module.exports = app;
