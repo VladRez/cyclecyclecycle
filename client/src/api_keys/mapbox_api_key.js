3487b59af3beef1aa9d852f3a2db9190095f471a
Robert <robertyeakeltroidl@gmail.com>
2019-08-31 16:17:19 -0700

    added map to create route page

---
+++ client/src/api_keys/mapbox_api_key.js
@@ -0,0 +1,2 @@
+module.exports =
+  "pk.eyJ1IjoiemVsb3hvbGV6IiwiYSI6ImNqenp4emFxZTE2cGozY2tpY3FjbXUyZmQifQ.Kz-wgvloKjZ9KbfvuzP88Q";
--- client/src/components/new_route.js
+++ client/src/components/new_route.js
@@ -1,11 +1,20 @@
 import React, { Component } from "react";
 import "./new_route.css";
+import ReactMapGL from "react-map-gl";
+import MapboxAPIKey from "../api_keys/mapbox_api_key";
 
 export default class NewRoute extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      search: ""
+      search: "",
+      viewport: {
+        width: "100%",
+        height: "100%",
+        latitude: 37.7577,
+        longitude: -122.4376,
+        zoom: 8
+      }
     };
 
     this.search = this.search.bind(this);
@@ -22,32 +31,41 @@ export default class NewRoute extends Component {
 
   render() {
     return (
-      <div className="map-nav-container">
-        <div className="map-nav">
-          <div className="map-nav-search">
-            <input
-              type="text"
-              value={this.state.search}
-              onChange={this.update("search")}
-              placeholder="Enter a location"
-              className="input search-input"
-            />
-            <button onClick={this.search} className="button search-button">
-              Search
-            </button>
-          </div>
+      <div>
+        <div className="map-nav-container">
+          <div className="map-nav">
+            <div className="map-nav-search">
+              <input
+                type="text"
+                value={this.state.search}
+                onChange={this.update("search")}
+                placeholder="Enter a location"
+                className="input search-input"
+              />
+              <button onClick={this.search} className="button search-button">
+                Search
+              </button>
+            </div>
 
-          <div>
-            <button className="button map-nav__button">Undo</button>
-            <button className="button map-nav__button">Clear</button>
-          </div>
+            <div>
+              <button className="button map-nav__button">Undo</button>
+              <button className="button map-nav__button">Clear</button>
+            </div>
 
-          <div>
-            <button className="button map-nav__button">Ride</button>
-            <button className="button map-nav__button">Run</button>
-          </div>
+            <div>
+              <button className="button map-nav__button">Ride</button>
+              <button className="button map-nav__button">Run</button>
+            </div>
 
-          <button className="button button-save">Save</button>
+            <button className="button button-save">Save</button>
+          </div>
+        </div>
+        <div className="mapbox-container">
+          <ReactMapGL
+            {...this.state.viewport}
+            mapboxApiAccessToken={MapboxAPIKey}
+            onViewportChange={viewport => this.setState({ viewport })}
+          />
         </div>
       </div>
     );
