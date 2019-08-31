aef3d83746f11b7ab914c09a282fa00130c73399
Robert <robertyeakeltroidl@gmail.com>
2019-08-31 16:48:04 -0700

    added navbar component

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,12 @@ import React from "react";
 import { Switch, Route, HashRouter } from "react-router-dom";
 import "./App.css";
 import NewRoute from "./components/new_route";
+import Navbar from "./components/navbar";
 
 function App() {
   return (
     <div>
+      <Navbar />
       <HashRouter>
         <Switch>
           <Route exact path="/routes/new" component={NewRoute} />
---
+++ client/src/components/navbar.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class Navbar extends Component {
+  render() {
+    return <div>navbar</div>;
+  }
+}
