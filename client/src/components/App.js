1ba8a645e737d73bbc420fa946602febe47dc43f
Vlad Rez <vladrezdev@gmail.com>
2019-08-31 14:54:34 -0700

    client index and app component setup

---
+++ client/src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      In App
+    </div>
+  );
+}
+
+export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,25 +1,78 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
 
+// Components
+import App from "./components/App";
 import ApolloClient from "apollo-client";
+
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { createHttpLink } from "apollo-link-http";
 import { ApolloProvider } from "react-apollo";
 import { onError } from "apollo-link-error";
+// eslint-disable-next-line
 import { ApolloLink } from "apollo-link";
 
-import Mutations from "./graphql/mutations"
+const { VERIFY_USER } = require("./graphql/mutations");
 
-console.log(`client running in ${process.env}`)
-
+// pass token within header
 const httpLink = createHttpLink({
-    uri: "http://localhost:5000/graphql",
-    headers: {
-        authorization: localStorage.getItem("auth-token") 
-    }
+  uri: "http://localhost:5000/graphql",
+  headers: {
+    authorization: localStorage.getItem("auth-token")
+  }
 });
 
+// verify login
+const token = localStorage.getItem("auth-token");
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const cache = new InMemoryCache({
+  dataIdFromObject: obj => obj._id || null
+});
+
+// eslint-disable-next-line
+const errorLink = onError(({ graphQLErrors }) => {
+  if (graphQLErrors) graphQLErrors.map(({ message }) => console.log(message));
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache,
+  onError: ({ networkError, graphQLErrors }) => {
+    console.log("graphQLErrors", graphQLErrors);
+    console.log("networkError", networkError);
+  }
+});
+
+cache.writeData({
+  data: {
+    isLoggedIn: Boolean(token)
+  }
+});
+
+if (token) {
+  client
+    .mutate({ mutation: VERIFY_USER, variables: { token } })
+    .then(({ data }) => {
+      cache.writeData({
+        data: {
+          isLoggedIn: data.verifyUser.loggedIn
+        }
+      });
+    });
+} else {
+  cache.writeData({
+    data: {
+      isLoggedIn: false
+    }
+  });
+}
+
+const Root = () => {
+  return (
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  );
+};
+
+ReactDOM.render(<Root />, document.getElementById("root"));
