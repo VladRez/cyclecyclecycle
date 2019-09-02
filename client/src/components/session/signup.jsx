9b2be6084ac08c5185757ffd06d73c84eea704bd
Robert <robertyeakeltroidl@gmail.com>
2019-09-02 01:00:22 -0700

    added css to login and signup pages

--- client/src/components/session/signup.jsx
+++ client/src/components/session/signup.jsx
@@ -1,3 +1,4 @@
+import "./session.css";
 import React from "react";
 import { Mutation } from "react-apollo";
 import Mutations from "../../graphql/mutations";
@@ -39,51 +40,64 @@ class Signup extends React.Component {
         update={(client, data) => this.updateCache(client, data)}
       >
         {register => (
-          <div>
-            <form
-              onSubmit={e => {
-                e.preventDefault();
-                register({
-                  variables: {
-                    fname: this.state.fname,
-                    lname: this.state.lname,
-                    email: this.state.email,
-                    password: this.state.password,
-                    password2: this.state.password2
-                  }
-                });
-              }}
-            >
-              <input
-                value={this.state.fname}
-                onChange={this.update("fname")}
-                placeholder="First Name"
-              />
-              <input
-                value={this.state.lname}
-                onChange={this.update("lname")}
-                placeholder="Last Name"
-              />
-              <input
-                value={this.state.email}
-                onChange={this.update("email")}
-                placeholder="Email"
-              />
-              <input
-                value={this.state.password}
-                onChange={this.update("password")}
-                type="password"
-                placeholder="Password"
-              />
-              <input
-                value={this.state.password2}
-                onChange={this.update("password2")}
-                type="password"
-                placeholder="Password"
-              />
-              <button type="submit">Sign Up</button>
-            </form>
-            <Link to="/login">Login</Link>
+          <div className="signup-page-background">
+            <div className="session-form-container">
+              <form
+                className="flex-column"
+                onSubmit={e => {
+                  e.preventDefault();
+                  register({
+                    variables: {
+                      fname: this.state.fname,
+                      lname: this.state.lname,
+                      email: this.state.email,
+                      password: this.state.password,
+                      password2: this.state.password2
+                    }
+                  });
+                }}
+              >
+                <h1 className="session-form-heading">Sign Up</h1>
+
+                <input
+                  value={this.state.fname}
+                  onChange={this.update("fname")}
+                  placeholder="First Name"
+                  className="session-form-input"
+                />
+                <input
+                  value={this.state.lname}
+                  onChange={this.update("lname")}
+                  placeholder="Last Name"
+                  className="session-form-input"
+                />
+                <input
+                  value={this.state.email}
+                  onChange={this.update("email")}
+                  placeholder="Email"
+                  className="session-form-input"
+                />
+                <input
+                  value={this.state.password}
+                  onChange={this.update("password")}
+                  type="password"
+                  placeholder="Password"
+                  className="session-form-input"
+                />
+                <input
+                  value={this.state.password2}
+                  onChange={this.update("password2")}
+                  type="password"
+                  placeholder="Password"
+                  className="session-form-input"
+                />
+                <button className="button session-form-button" type="submit">
+                  Sign Up
+                </button>
+              </form>
+              <span className="margin-right-s">Already have an account?</span>
+              <Link to="/login">Login</Link>
+            </div>
           </div>
         )}
       </Mutation>
