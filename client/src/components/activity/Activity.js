376aaa6973f3e03ef9950c4c7082bec341730215
Robert <robertyeakeltroidl@gmail.com>
2019-09-04 11:07:29 -0700

    added styling and html structure for activites

--- client/src/components/activity/Activity.js
+++ client/src/components/activity/Activity.js
@@ -23,42 +23,192 @@ class Activity extends React.Component {
       this.setState({ [type]: e.target.value });
     };
   }
-    // handleSubmit(e) {
-    //     e.preventDefault();
-    //     this.props.(this.state);
-    //     form.reset(); 
-    // }
+  // handleSubmit(e) {
+  //     e.preventDefault();
+  //     this.props.(this.state);
+  //     form.reset();
+  // }
   render() {
     return (
-      <form>
-        Distance
-        <input type="text" onChange={this.handleChange("distance")} />
-        Duration
-        <input type="text" onChange={this.handleChange("duration")} />
-        Elevation
-        <input type="text" onChange={this.handleChange("elevation")} />
-        Sport
-        <input type="text" onChange={this.handleChange("sport")} />
-        Date and Time 
-        <input type="text" onChange={this.handleChange("date")}  />
-        Title 
-        <input type="text" onChange= {this.handleChange("title")}/>    
-        Run Type  
-        <input type="text" onChange={this.handleChange("runtype")} />
-        Tags 
-        <input type="text" onChange={this.handleChange("tags")} />
-        Description  
-        <input type="text" onChange={this.handleChange("description")} /> 
-            {/* <button onSubmit={this.handleSubmit()}> Submit</button>    */}
+      // <div>
+      //   <form>
+      //     Distance
+      //     <input type="text" onChange={this.handleChange("distance")} />
+      //     Duration
+      //     <input type="text" onChange={this.handleChange("duration")} />
+      //     Elevation
+      //     <input type="text" onChange={this.handleChange("elevation")} />
+      //     Sport
+      //     <input type="text" onChange={this.handleChange("sport")} />
+      //     Date and Time
+      //     <input type="text" onChange={this.handleChange("date")} />
+      //     Title
+      //     <input type="text" onChange={this.handleChange("title")} />
+      //     Run Type
+      //     <input type="text" onChange={this.handleChange("runtype")} />
+      //     Tags
+      //     <input type="text" onChange={this.handleChange("tags")} />
+      //     Description
+      //     <input type="text" onChange={this.handleChange("description")} />
+      //     {/* <button onSubmit={this.handleSubmit()}> Submit</button>    */}
+      //   </form>
+
+      //   {/* <Mutation
+      //   //     mutation={ADD}
+      //   //     onCompleted={data => {
+      //   //         console.log(data)
+      //   //     }}
+      //   // >
+      //   //     {(registerUser) => { }}
+      //   // </Mutation> */}
+
+      <div className="page-container">
+        <form>
+          <div className="activity-form-container">
+            <div className="activity-form-section">
+              <h1 className="activity-form-heading">Manual Entry</h1>
+            </div>
+            <div>
+              <div>
+                <div>
+                  <label className="label">Distance</label>
+                </div>
+                <div>
+                  <div>
+                    <input className="input" />
+                  </div>
+                  <div>
+                    <input className="input" />
+                  </div>
+                </div>
+              </div>
+
+              <div>
+                <div>
+                  <label className="label">Duration</label>
+                </div>
+                <div>
+                  <div>
+                    <div>
+                      <label className="label">hr</label>
+                    </div>
+                    <div>
+                      <input className="input" />
+                    </div>
+                  </div>
+                  <div>
+                    <div>
+                      <label className="label">min</label>
+                    </div>
+                    <div>
+                      <input className="input" />
+                    </div>
+                  </div>
+                  <div>
+                    <div>
+                      <label className="label">s</label>
+                    </div>
+                    <div>
+                      <input className="input" />
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div>
+                <div>
+                  <label className="label">Elevation</label>
+                </div>
+                <div>
+                  <div>
+                    <div>
+                      <input className="input" />
+                    </div>
+                    <div>
+                      <input className="input" />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="activity-form-section">
+              <div>
+                <div>
+                  <div>
+                    <label className="label">Sport</label>
+                  </div>
+                  <div>
+                    <input className="input" />
+                  </div>
+                </div>
+
+                <div>
+                  <div>
+                    <label className="label">Date & Time</label>
+                  </div>
+                  <div>
+                    <div>
+                      <input className="input" />
+                    </div>
+                    <div>
+                      <input className="input" />
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div>
+                <div>
+                  <div>
+                    <label className="label">Title</label>
+                  </div>
+                  <div>
+                    <input className="input" />
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div>
+              <div>
+                <div>
+                  <div>
+                    <label className="label">Ride Type</label>
+                  </div>
+                  <div>
+                    <input className="input" />
+                  </div>
+                </div>
+              </div>
+
+              <div>
+                <div>
+                  <label className="label">Description</label>
+                </div>
+                <div>
+                  <textarea></textarea>
+                </div>
+              </div>
+
+              <div>
+                <div>
+                  <label className="label">
+                    Privacy Controls - who can see?
+                  </label>
+                  <div>
+                    <input className="input" />
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div>
+              <button className="button button-primary">Create</button>
+            </div>
+          </div>
         </form>
-        // <Mutation
-        //     mutation={ADD}
-        //     onCompleted={data => {
-        //         console.log(data)
-        //     }}
-        // >
-        //     {(registerUser) => { }}
-        // </Mutation>
+      </div>
     );
   }
 }
--- client/src/components/activity_show/activity_show.jsx
+++ client/src/components/activity_show/activity_show.jsx
@@ -1,83 +1,130 @@
 import React, { Component } from "react";
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
+import Queries from "../../graphql/queries";
+import { ApolloConsumer } from "react-apollo";
 import "./activity_show.css";
+
+const ACTIVITY_QUERY = gql`
+  query {
+    activity(_id: "5d6d78db4aff4475172c67b2") {
+      distance
+      duration
+      elevation
+      sport
+      date
+      time
+      title
+      runtype
+      tags
+      description
+      privacycontrols
+    }
+  }
+`;
 
 export default class ActivityShow extends Component {
   render() {
     return (
-      <div className="page-container">
-        <div className="activity-item">
-          <div>
-            <div className="activity-item-nav font-xl flex-row">
-              <div className="group-row-s padding-m">
-                <span className="activity-item-name">*robert yeakel*</span>
-                <span className="activity-item-activity-type">*Run*</span>
-              </div>
-              <div className=" flex-row">
-                <a className="padding-m icon-button">
-                  <i class="fas fa-pen activity-show-navbar-icon"></i>
-                </a>
-                <a className="padding-m icon-button">
-                  <i class="fas fa-trash activity-show-navbar-icon"></i>
-                </a>
-              </div>
-            </div>
+      <ApolloConsumer>
+        {client => {
+          debugger;
+          return (
+            <Query query={ACTIVITY_QUERY}>
+              {({ loading, error, data }) => {
+                debugger;
+                if (loading) return <div>Loading</div>;
+                if (error) return <div>Error</div>;
 
-            <div className="activity-item-body flex-row">
-              <div className="activity-item-section-1">
-                <div className="flex-row">
-                  {/* <div className="activity-item-avatar">
+                return (
+                  <div className="page-container">
+                    <div className="activity-item">
+                      <div>
+                        <div className="activity-item-nav font-xl flex-row">
+                          <div className="group-row-s padding-m">
+                            <span className="activity-item-name">
+                              *robert yeakel*
+                            </span>
+                            <span className="activity-item-activity-type">
+                              {data.activity.sport}
+                            </span>
+                          </div>
+                          <div className=" flex-row">
+                            <a className="padding-m icon-button">
+                              <i class="fas fa-pen activity-show-navbar-icon"></i>
+                            </a>
+                            <a className="padding-m icon-button">
+                              <i class="fas fa-trash activity-show-navbar-icon"></i>
+                            </a>
+                          </div>
+                        </div>
+
+                        <div className="activity-item-body flex-row">
+                          <div className="activity-item-section-1">
+                            <div className="flex-row">
+                              {/* <div className="activity-item-avatar">
                     <i class="fas fa-user font-size-l"></i>
                   </div> */}
-                  <div>
-                    <div className="font-xs line-height-xs font-weight-medium font-color-light">
-                      *4:00 PM on Sunday, September 1, 2019*
-                    </div>
-                    <div className="font-xxl line-height-xxl font-weight-dark">
-                      *Title field*
-                    </div>
-                    <div className="font-s line-height-s">
-                      *Description field Description field Description field
-                      Description field Description field Description field
-                      Description field Description field *
+                              <div>
+                                <div className="font-xs line-height-xs font-weight-medium font-color-light">
+                                  {data.activity.date}
+                                </div>
+                                <div className="font-xxl line-height-xxl font-weight-dark">
+                                  {data.activity.title}
+                                </div>
+                                <div className="font-s line-height-s">
+                                  {data.activity.description}
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                          <div className="activity-item-section-2">
+                            <div className="flex-row group-row-xl activity-item-stats margin-bottom-m">
+                              <div>
+                                <div className="font-xxl">
+                                  {data.activity.distance}
+                                </div>
+                                <div className="font-xs font-color-light font-weight-medium">
+                                  Distance
+                                </div>
+                              </div>
+                              <div>
+                                <div className="font-xxl">
+                                  {data.activity.duration}
+                                </div>
+                                <div className="font-xs font-color-light font-weight-medium">
+                                  Duration
+                                </div>
+                              </div>
+                              <div>
+                                <div className="font-xxl">*6:00/mi*</div>
+                                <div className="font-xs font-color-light font-weight-medium">
+                                  Pace
+                                </div>
+                              </div>
+                              <div>
+                                <div className="font-xxl">
+                                  {data.activity.elevation}
+                                </div>
+                                <div className="font-xs font-color-light font-weight-medium">
+                                  Elevation
+                                </div>
+                              </div>
+                            </div>
+                            <div>
+                              <div>*Shoes: Nike Free RN (35.0 mi)*</div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
                     </div>
                   </div>
-                </div>
-              </div>
-              <div className="activity-item-section-2">
-                <div className="flex-row group-row-xl activity-item-stats margin-bottom-m">
-                  <div>
-                    <div className="font-xxl">*15.00 mi*</div>
-                    <div className="font-xs font-color-light font-weight-medium">
-                      Distance
-                    </div>
-                  </div>
-                  <div>
-                    <div className="font-xxl">*1:30:00*</div>
-                    <div className="font-xs font-color-light font-weight-medium">
-                      Duration
-                    </div>
-                  </div>
-                  <div>
-                    <div className="font-xxl">*6:00/mi*</div>
-                    <div className="font-xs font-color-light font-weight-medium">
-                      Pace
-                    </div>
-                  </div>
-                  <div>
-                    <div className="font-xxl">*150ft*</div>
-                    <div className="font-xs font-color-light font-weight-medium">
-                      Elevation
-                    </div>
-                  </div>
-                </div>
-                <div>
-                  <div>*Shoes: Nike Free RN (35.0 mi)*</div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+                );
+              }}
+            </Query>
+          );
+        }}
+      </ApolloConsumer>
     );
   }
 }
