c46537d892f10f3faaf92dd8adfd08af8f563f2e
Vlad Rez <vladrezdev@gmail.com>
2019-09-04 00:22:16 -0700

    add travel mode switch feature

--- client/src/components/route/route_map.jsx
+++ client/src/components/route/route_map.jsx
@@ -59,6 +59,7 @@ class RouteMap extends React.Component {
     this.clearMap = this.clearMap.bind(this);
   }
 
+
   componentDidMount() {
     const map = this.refs.map;
     this.map = new window.google.maps.Map(map, mapOptions);
@@ -94,13 +95,14 @@ class RouteMap extends React.Component {
     });
   }
 
-  calculateAndDisplayRoute() {
-    let origin = this.state.routes[0].location;
-    let destination = this.state.routes[this.state.routes.length - 1].location;
-    let waypoints = this.state.routes.slice(1, this.state.routes.length - 1);
-    let travelMode = this.state.TravelMode; //window.google.maps.TravelMode.WALKING;
-    let optimizeWaypoints = false;
-
+  calculateAndDisplayRoute(opt = {}) {
+  
+    let origin = opt.origin || this.state.routes[0].location;
+    let destination = opt.destination || this.state.routes[this.state.routes.length - 1].location;
+    let waypoints = opt.waypoints || this.state.routes.slice(1, this.state.routes.length - 1);
+    let travelMode = opt.travelMode || this.state.TravelMode; //window.google.maps.TravelMode.WALKING;
+    let optimizeWaypoints = opt.optimizeWaypoints || false;
+      
     let options = {
       origin,
       destination,
@@ -111,7 +113,8 @@ class RouteMap extends React.Component {
 
     this.directionsService.route(options, (res, status) => {
       if (status === "OK") {
-        this.setState({ route_details: res.routes[0].legs[0] });
+
+        this.setState({route_details: res.routes[0].legs[0] });
         if (this.state.routeType === "polyline") {
           this.renderPolyLine(res);
         } else {
@@ -141,6 +144,23 @@ class RouteMap extends React.Component {
   closeModal() {
     this.setState({ modal: false });
   }
+
+  changeTravelMode() {
+
+    if (this.state.TravelMode === "WALKING") {
+      this.setState({TravelMode: "BICYCLING"}) 
+      if (this.state.routes.length) this.calculateAndDisplayRoute({travelMode: "BICYCLING"})
+      
+      }
+      else {
+        this.setState({TravelMode: "WALKING"})
+      if (this.state.routes.length) this.calculateAndDisplayRoute({travelMode: "WALKING"})
+      }
+
+    
+  }
+
+
   render() {
     let distance = this.state.route_details.distance
       ? this.state.route_details.distance.text
@@ -150,6 +170,7 @@ class RouteMap extends React.Component {
       ? this.state.route_details.duration.text
       : "0";
     let RouteType = this.state.TravelMode === "WALKING" ? "Run" : "Ride";
+    let OtherRouteType = this.state.TravelMode === "WALKING" ? "Ride" : "Run";
 
     let modal = this.state.modal ? (
       <div className="modal">
@@ -180,6 +201,7 @@ class RouteMap extends React.Component {
           <div className="toolControls">
             <button onClick={() => this.clearMap()}>X</button>
             <button onClick={() => this.openModal()}>save</button>
+            <button onClick={() => {this.changeTravelMode()}}>{OtherRouteType}</button>
           </div>
         </div>
         <div className="mapCanvas" ref="map"></div>
