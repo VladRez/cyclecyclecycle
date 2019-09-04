7a9dfc0da300642ad644961cc21145ac0291cf22
Vlad Rez <vladrezdev@gmail.com>
2019-09-03 20:01:39 -0700

    fix map api polyline re-rendering overlap

--- client/src/components/route/route_map.jsx
+++ client/src/components/route/route_map.jsx
@@ -8,7 +8,37 @@ const mapOptions = {
   }, // San Francisco coords
   zoom: 15,
   mapTypeId: "terrain",
-  disableDefaultUI: true
+  disableDefaultUI: true,
+  styles: [
+    {
+      featureType: "poi.business",
+      stylers: [{ visibility: "off" }]
+    },
+    {
+      featureType: "poi.government",
+      stylers: [{ visibility: "off" }]
+    },
+    {
+      featureType: "poi.attraction",
+      stylers: [{ visibility: "off" }]
+    },
+    {
+      featureType: "poi.medical",
+      stylers: [{ visibility: "off" }]
+    },
+    {
+      featureType: "poi.school",
+      stylers: [{ visibility: "off" }]
+    },
+    {
+      featureType: "poi.place_of_worship",
+      stylers: [{ visibility: "off" }]
+    },
+    {
+      featureType: "transit",
+      stylers: [{ visibility: "off" }]
+    }
+  ]
 };
 
 class RouteMap extends React.Component {
@@ -17,24 +47,39 @@ class RouteMap extends React.Component {
     // this.routes = [];
     this.state = {
       routes: [],
-      TravelMode: 'WALKING'
-    }
+      TravelMode: "WALKING",
+      routeType: "polyline"
+    };
+
+    const directionsService = new window.google.maps.DirectionsService();
+    this.directionsService = directionsService;
+
+    this.clearMap = this.clearMap.bind(this)
+
+    
   }
-
-
 
   componentDidMount() {
-    this.initMap()
-  }
-
-  initMap(){
     const map = this.refs.map;
     this.map = new window.google.maps.Map(map, mapOptions);
     const directionsDisplay = new window.google.maps.DirectionsRenderer();
-    const directionsService = new window.google.maps.DirectionsService();
+    this.directionsDisplay = directionsDisplay;
+    this.directionsDisplay.setMap(this.map);
+    this.handleClick();
 
-    directionsDisplay.setMap(this.map);
+    const directionPolyLine = new window.google.maps.Polyline({
+      path: [],
+      geodesic: true,
+      strokeColor: "#444444",
+      strokeOpacity: 0.5,
+      strokeWeight: 5
+    });
 
+    this.directionPolyLine = directionPolyLine
+    this.directionPolyLine.setMap(this.map)
+  }
+
+  handleClick() {
     window.google.maps.event.addListener(this.map, "click", event => {
       let point = {
         location: new window.google.maps.LatLng(
@@ -43,26 +88,17 @@ class RouteMap extends React.Component {
         ),
         stopover: false
       };
-      // this.routes.push(point);
-      this.setState({routes:[...this.state.routes, point]})
-      // let m = new window.google.maps.Marker({
-      //   position: point.location,
-      //   map: this.map
-      // });
-      this.calculateAndDisplayRoute(
-        directionsService,
-        directionsDisplay,
-        this.state.routes
-      );
+      this.setState({ routes: [...this.state.routes, point] });
+
+      this.calculateAndDisplayRoute();
     });
   }
 
-
-  calculateAndDisplayRoute(directionsService, directionsDisplay, routes) {
-    let origin = routes[0].location;
-    let destination = routes[routes.length - 1].location;
-    let waypoints = routes.slice(1, routes.length - 1);
-    let travelMode = window.google.maps.TravelMode.WALKING;
+  calculateAndDisplayRoute() {
+    let origin = this.state.routes[0].location;
+    let destination = this.state.routes[this.state.routes.length - 1].location;
+    let waypoints = this.state.routes.slice(1, this.state.routes.length - 1);
+    let travelMode = this.state.TravelMode; //window.google.maps.TravelMode.WALKING;
     let optimizeWaypoints = false;
 
     let options = {
@@ -73,47 +109,53 @@ class RouteMap extends React.Component {
       travelMode
     };
 
-    // const directionPolyLine = new window.google.maps.Polyline({
-    //   path: routes.map(route => route.location),
-    //   geodesic: true,
-    //   strokeColor: "#444444",
-    //   strokeOpacity: 0.9,
-    //   strokeWeight: 4
-    // });
-    // directionPolyLine.setMap(this.map);
-    directionsService.route(options, (res, status) => {
+    this.directionsService.route(options, (res, status) => {
       if (status === "OK") {
-        // directionsDisplay.setDirections(res);
-        const directionPolyLine = new window.google.maps.Polyline({
-          path: res.routes[res.routes.length - 1].overview_path,
-          geodesic: true,
-          strokeColor: "#444444",
-          strokeOpacity: 0.9,
-          strokeWeight: 4
-        });
-        directionPolyLine.setMap(this.map);
+        if (this.state.routeType === "polyline") {
+          this.renderPolyLine(res);
+        } else {
+          this.directionsDisplay.setDirections(res);
+        }
       } else {
         console.log("error " + status);
       }
     });
   }
 
+  renderPolyLine(res) {
+    // this.directionPolyLine.setMap(null);
+    this.directionPolyLine.setPath(res.routes[0].overview_path);
+  }
+
+  clearMap(){
+
+    
+  }
   render() {
     return (
-     <div>
+      <div>
         <button
-      style={{zindex: 2}}
-      onClick={()=>{this.setState({routes:[]});this.initMap()}}>clear</button>
-        <div
-          style={{ position: "absolute", width: "100%", margin: "auto", height: "100%" }}
-          ref="map"
+          style={{ zindex: 2 }}
+          onClick={()=>this.clearMap()}
         >
-      
-        </div>
-     </div>
-
+          clear
+        </button>
+        <div
+          style={{
+            position: "absolute",
+            width: "100%",
+            margin: "auto",
+            height: "100%"
+          }}
+          ref="map"
+        ></div>
+      </div>
     );
   }
 }
 
 export default withRouter(RouteMap);
+// let m = new window.google.maps.Marker({
+//   position: point.location,
+//   map: this.map
+// });
