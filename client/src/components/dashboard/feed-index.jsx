8f97835c0f8ed9eec8e1a3edebd679338d5c1dd9
Vlad Rez <vladrezdev@gmail.com>
2019-09-26 17:34:20 -0700

    add no activity image to feed-index

--- client/src/components/dashboard/feed-index.jsx
+++ client/src/components/dashboard/feed-index.jsx
@@ -49,17 +49,42 @@ export default class FeedIndex extends Component {
               fetchPolicy="no-cache"
             >
               {({ loading, error, data }) => {
-              
                 if (loading) return <Loader />;
                 if (error) return <div>Error</div>;
-                
+
                 console.log("feed-index");
                 if (data.activity_by_user.length === 0)
-                  return <div>Add your first Activity</div>;
+                  return (
+                    <div className="cta-card">
+                      <div className="cta-card__header">
+                        <img className="cta-card__img" src={CTACardImg}></img>
+                      </div>
+                      <div className="cta-card__body">
+                        <h1 className="cta-card__heading">Get Started</h1>
+                        <p className="cta-card__description">
+                          Plan and record your workouts by creating an activity!
+                        </p>
+                        <div className="cta-card__footer">
+                          <Link
+                            to="activity/new"
+                            className="button button-primary"
+                          >
+                            Create an Activity
+                          </Link>
+                        </div>
+                      </div>
+                    </div>
+                  );
                 else {
                   return data.activity_by_user
                     .reverse()
-                    .map(act => <FeedIndexItem user={this.props.user} activity={act} key={act._id} />);
+                    .map(act => (
+                      <FeedIndexItem
+                        user={this.props.user}
+                        activity={act}
+                        key={act._id}
+                      />
+                    ));
                 }
               }}
             </Query>
